ba8afbd347eb78519d0ee0ff05f3063822694a3c
Laura González <e_lgon01@mercadona.es>
2021-02-28 20:56:49 +0100

    feature: added uiview animation

--- TrendingRepos/Presentation/Modules/Home/HomeViewController.swift
+++ TrendingRepos/Presentation/Modules/Home/HomeViewController.swift
@@ -15,8 +15,10 @@ final class HomeViewController: UIViewController {
     @IBOutlet weak var enterButton: Button!
     @IBOutlet weak var termsLabel: Label!
     @IBOutlet weak var webLabel: Label!
-    
+
     @IBOutlet weak var logoTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var enterButtonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var enterButtonHeightConstraint: NSLayoutConstraint!
 
     var viewModel: HomeViewModelProtocol!
 
@@ -36,8 +38,42 @@ final class HomeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isNavigationBarHidden = true
-        if view.bounds.height < 828 {
+        if self.view.bounds.height < 828 {
             logoTopConstraint.constant = 28
+        } else {
+            logoTopConstraint.constant = 120
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateButton()
+    }
+
+    private func animateButton() {
+        enterButtonBottomConstraint.constant = 16
+        enterButtonHeightConstraint.constant = 60
+
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.2,
+                       usingSpringWithDamping: 0.4,
+                       initialSpringVelocity: 0.7,
+                       options: .curveEaseInOut) { [weak self] in
+            guard let self = self else { return }
+            self.view.layoutIfNeeded()
+        } completion: { [weak self] completion in
+            self?.enterButtonHeightConstraint.constant = 48
+
+            UIView.animate(withDuration: 0.4,
+                           delay: 0.2,
+                           usingSpringWithDamping: 0.4,
+                           initialSpringVelocity: 0.3,
+                           options: .curveEaseInOut) { [weak self] in
+                guard let self = self else { return }
+                self.view.layoutIfNeeded()
+            } completion: { completion in
+
+            }
         }
     }
 
